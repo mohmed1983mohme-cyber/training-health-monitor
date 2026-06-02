264c0fc583377b4b0dd2599c18bf94a98160e79c
mohmed1983mohme-cyber <mohmed1983mohme@gmail.com>
2026-06-02 03:20:14 +0100

    Add training_monitor package initialization

---
+++ training_monitor/__init__.py
@@ -0,0 +1,10 @@
+"""
+Training Health Monitor - Comprehensive framework for monitoring neural network training
+"""
+
+__version__ = "0.1.0"
+__author__ = "Mohmed1983"
+
+from .monitor import TrainingHealthMonitor
+
+__all__ = ['TrainingHealthMonitor']
